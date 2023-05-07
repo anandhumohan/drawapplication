8a12c6b0417f836dddeda8f760608844cdf8822a
anandhumohan <anandhumohan5@gmail.com>
2023-05-07 14:05:26 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProjectNameModalComponent } from './components/project-name-modal/project-name-modal.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [AppComponent, ProjectNameModalComponent],
   imports: [
     BrowserModule,
+    HttpClientModule,
     IonicModule.forRoot(),
     AppRoutingModule,
     ReactiveFormsModule
