e05a3426f51b434e0735de76fb49484f295243c9
anandhumohan <anandhumohan5@gmail.com>
2023-05-14 14:06:18 +0100

    plus button added

--- src/app/project-view/project-view.page.ts
+++ src/app/project-view/project-view.page.ts
@@ -13,6 +13,13 @@ import { SubNodeCreationComponent } from '../components/sub-node-creation/sub-no
 export class ProjectViewPage implements OnInit {
   projectTitle = '';
   showSubNodeCreation = false;
+  subNodes: any[] = [];
+
+  centerX = window.innerWidth / 2;
+  centerY = window.innerHeight / 2; // center of the parent node
+  radius = 300; // distance from the parent to a subnode
+
+
 
   constructor(private activatedRoute: ActivatedRoute, private alertController: AlertController, private modalController: ModalController) {}
 
@@ -33,8 +40,54 @@ export class ProjectViewPage implements OnInit {
         projectTitle: projectTitle
       }
     });
+
+    modal.onDidDismiss().then((data) => {
+      // Check if data is provided, and if so, add the subnode to the subNodes array
+      if (data.data) {
+        this.subNodes.push(data.data);
+      }
+    });
+
     return await modal.present();
   }
+
+  getX2(i: number): number {
+    const angle = 2 * Math.PI * i / this.subNodes.length; // Remove the angle adjustment
+    return this.centerX + this.radius * Math.cos(angle);
+  }
+  
+  getY2(i: number): number {
+    const angle = 2 * Math.PI * i / this.subNodes.length; // Remove the angle adjustment
+    return this.centerY + this.radius * Math.sin(angle);
+  }
+  
+  
+  getLineStartX(i: number): number {
+    const angle = 2 * Math.PI * i / this.subNodes.length;
+    return this.centerX + 100 * Math.cos(angle);
+  }
+  
+  getLineStartY(i: number): number {
+    const angle = 2 * Math.PI * i / this.subNodes.length;
+    return this.centerY + 100 * Math.sin(angle);
+  }
+  
+  getLineEndX(i: number): number {
+    const angle = 2 * Math.PI * i / this.subNodes.length;
+    return this.getX2(i) + 50 * Math.cos(angle);
+  }
+  
+  getLineEndY(i: number): number {
+    const angle = 2 * Math.PI * i / this.subNodes.length;
+    return this.getY2(i) + 50 * Math.sin(angle);
+  }
+  
+  
+  
+  
+  
+  
+  
 
   
 
